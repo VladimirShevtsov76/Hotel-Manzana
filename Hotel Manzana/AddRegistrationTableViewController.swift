d88cb3ca5e8e7f72b748a3a546fa8932de67cd19
Denis Bystruev <dbystruev@me.com>
2018-10-18 21:45:15 +0300

    Added min/current date pickers restrictions

--- Hotel Manzana/AddRegistrationTableViewController.swift
+++ Hotel Manzana/AddRegistrationTableViewController.swift
@@ -13,8 +13,28 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        updateDateViews()
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(60 * 60 * 24)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
     }
     
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
