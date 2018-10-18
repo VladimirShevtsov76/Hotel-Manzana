a85c1ae1b05a1cbf37e8e4eb8c8024e443a879e0
Denis Bystruev <dbystruev@me.com>
2018-10-18 22:52:45 +0300

    Implemented date picker hide / show

--- Hotel Manzana/AddRegistrationTableViewController.swift
+++ Hotel Manzana/AddRegistrationTableViewController.swift
@@ -18,6 +18,21 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var checkOutDateLabel: UILabel!
     @IBOutlet weak var checkOutDatePicker: UIDatePicker!
     
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDatePickerShown: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,14 +52,74 @@ class AddRegistrationTableViewController: UITableViewController {
         checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath:
+            if isCheckInDatePickerShown {
+                print(#function, checkInDatePicker.frame.height)
+                return 216
+            } else {
+                return 0
+            }
+        case checkOutDatePickerCellIndexPath:
+            if isCheckOutDatePickerShown {
+                print(#function, checkOutDatePicker.frame.height)
+                return 216
+            } else {
+                return 0
+            }
+        default:
+            return 44
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+            } else if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+        default:
+            isCheckInDatePickerShown = false
+            isCheckOutDatePickerShown = false
+        }
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
         
         print(#function)
         print("First Name: \(firstName)")
         print("Last Name: \(lastName)")
         print("E-mail: \(email)")
+        print("Check In: \(checkInDate)")
+        print("Check Out: \(checkOutDate)")
+    }
+    
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
     }
 }
